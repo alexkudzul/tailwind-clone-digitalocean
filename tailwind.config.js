5f7ef6d9e49c477d123d7f1eca0edbb76c9ebbbf
alexkudzul <alexkudzul@gmail.com>
2023-03-17 10:58:55 -0600

    Config font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
    utility class (html, js, vue, etc) para ser compiladas */
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        /** sans es el font por defecto que utiliza tailwind, se sobrescribirá con
+         la que se va a utilizar de DigitalOcean ('Work Sans', sans-serif) */
+        sans: "'Work Sans', sans-serif",
+        cascadia: '"CascadiaCode"',
+      },
+      colors: {
+        'do-blue-dark': '#080c2d',
+        'do-blue-mediumdark': '#1d274c',
+        'do-blue-medium': 'rgb(20, 86, 255)',
+        'do-blue-light': 'rgb(0, 105, 255)',
+        'do-blue-lighter': '#f9fafe',
+      },
+    },
   },
   plugins: [],
 };
