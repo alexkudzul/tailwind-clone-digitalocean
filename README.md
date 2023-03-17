b89f81a402137d351d4f227cba67cc5a183d4083
alexkudzul <alexkudzul@gmail.com>
2023-03-16 19:56:06 -0600

    Install tailwind with vite

---
+++ README.md
@@ -0,0 +1,13 @@
+## Instalación
+1. Clonar el repositorio localmente.
+2. Instalar las dependencias.
+
+```
+npm install
+```
+
+3. Ejecute su proceso de compilación.
+```
+npm run dev
+```
+4. Abrir en un navegador la dirección [http://localhost:port](http://localhost:port)
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /**  content: indica a tailwind donde se encuentra los archivos que contiene las
+   utility class (html, js, vue, etc) para ser compiladas */
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
